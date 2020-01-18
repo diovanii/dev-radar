155789750c07b0e33b3d18c9dd3aae3cc8d2d9c2
Diovani <diovanidalmoro28@gmail.com>
2020-01-18 16:09:14 -0300

    App works in real time :arrows_counterclockwise:

--- backend/src/controllers/SearchController.js
+++ backend/src/controllers/SearchController.js
@@ -22,7 +22,6 @@ module.exports = {
       }
     })
 
-    console.log(devs)
     return response.json(devs)
   }
 }
--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -5,6 +5,7 @@ import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
 import { MaterialIcons } from '@expo/vector-icons'
 
 import api from '../services/api'
+import { connect, disconnect, subscribeToNewDevs } from '../services/socket'
 
 function Main({ navigation }) {
   const [ devs, setDevs ] = useState([])
@@ -34,6 +35,22 @@ function Main({ navigation }) {
     loadInitialPosition()
   }, [])
 
+  useEffect(() => {
+    subscribeToNewDevs(dev => setDevs([...devs, dev]))
+  }, [devs])
+
+  function setupWebSocket() {
+    disconnect()
+
+    const { latitude, longitude } = currentRegion
+
+    connect(
+      latitude,
+      longitude,
+      techs
+    )
+  }
+
   async function loadDevs() {
     const { latitude, longitude } = currentRegion
 
@@ -46,6 +63,7 @@ function Main({ navigation }) {
     })
 
     setDevs(response.data)
+    setupWebSocket()
   }
 
   function handleRegionChanged(region) {
@@ -105,7 +123,7 @@ const styles = StyleSheet.create({
   avatar: {
     width: 54,
     height: 54,
-    borderRadius: 5,
+    borderRadius: 100,
     borderWidth: 5,
     borderColor: '#FFF'
   },
